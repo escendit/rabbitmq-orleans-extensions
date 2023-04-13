f9bbc4ec9bf817b6aad26bc9f48c3520bb1eeba6
Simon Novak <snovak7@gmail.com>
2023-04-13 13:58:37 +0200

    Setup solution

---
+++ src/Escendit.Orleans.Streaming.RabbitMQ/DefaultRabbitMQAdapter.cs
@@ -0,0 +1,22 @@
+﻿using Orleans.Runtime;
+using Orleans.Streams;
+
+namespace Escendit.Orleans.Streaming.RabbitMQ;
+
+public class DefaultRabbitMQAdapter : IQueueAdapter
+{
+    public Task QueueMessageBatchAsync<T>(StreamId streamId, IEnumerable<T> events, StreamSequenceToken token,
+        Dictionary<string, object> requestContext)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IQueueAdapterReceiver CreateReceiver(QueueId queueId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public string Name { get; } = string.Empty;
+    public bool IsRewindable { get; } = false;
+    public StreamProviderDirection Direction { get; } = StreamProviderDirection.ReadWrite;
+}
