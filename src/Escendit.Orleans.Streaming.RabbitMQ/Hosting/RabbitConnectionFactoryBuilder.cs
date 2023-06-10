e1622dcaa12ae1961704ef555c539059dde75589
Simon Novak <snovak7@gmail.com>
2023-06-10 21:21:40 +0200

    fix(#2): Implementation RabbitConnectionFactoryBuilder.cs

---
+++ src/Escendit.Orleans.Streaming.RabbitMQ/Hosting/RabbitConnectionFactoryBuilder.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) Escendit Ltd. All Rights Reserved.
+// Licensed under the MIT. See LICENSE.txt file in the solution root for full license information.
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+using Escendit.Orleans.Streaming.RabbitMQ.Hosting;
+
+/// <summary>
+/// Rabbit Connection Factory Builder.
+/// </summary>
+public class RabbitConnectionFactoryBuilder : IRabbitConnectionFactoryBuilder
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="RabbitConnectionFactoryBuilder"/> class.
+    /// </summary>
+    /// <param name="services">The service collection.</param>
+    /// <param name="name">The name.</param>
+    internal RabbitConnectionFactoryBuilder(IServiceCollection services, string name)
+    {
+        Services = services;
+        Name = name;
+    }
+
+    /// <inheritdoc />
+    public IServiceCollection Services { get; }
+
+    /// <inheritdoc />
+    public string Name { get; }
+}
