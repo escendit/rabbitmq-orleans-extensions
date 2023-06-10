e9b3439fc8aad83361c14513c9307c1150641984
Simon Novak <snovak7@gmail.com>
2023-06-10 21:19:22 +0200

    fix(#2): Additiona overloads for NamingUtility.cs

--- src/Escendit.Orleans.Streaming.RabbitMQ/Core/NamingUtility.cs
+++ src/Escendit.Orleans.Streaming.RabbitMQ/Core/NamingUtility.cs
@@ -11,15 +11,29 @@ using global::Orleans.Streams;
 /// </summary>
 internal static class NamingUtility
 {
+    private const string Stream = "stream";
+    private const string Queue = "queue";
+
     /// <summary>
-    /// Create a Name for a Queue.
+    /// Create a Name for a Queue or an Exchange.
     /// </summary>
     /// <param name="clusterOptions">The cluster options.</param>
     /// <param name="queueId">The queue id.</param>
     /// <returns>The unique name for a queue or an exchange.</returns>
     public static string CreateNameForQueue(ClusterOptions clusterOptions, QueueId queueId)
     {
-        return $"queue-{clusterOptions.ClusterId}-{clusterOptions.ServiceId}-{queueId.ToString()}";
+        return CreateNameInternal(Queue, clusterOptions.ClusterId, clusterOptions.ServiceId, queueId.ToString());
+    }
+
+    /// <summary>
+    /// Create a Name for a Queue or an Exchange.
+    /// </summary>
+    /// <param name="clusterOptions">The cluster options.</param>
+    /// <param name="name">The exchange name.</param>
+    /// <returns>The unique name for an exchange.</returns>
+    public static string CreateNameForQueue(ClusterOptions clusterOptions, string name)
+    {
+        return CreateNameInternal(Queue, clusterOptions.ClusterId, clusterOptions.ServiceId, name);
     }
 
     /// <summary>
@@ -30,6 +44,11 @@ internal static class NamingUtility
     /// <returns>The unique name for a stream (queue).</returns>
     public static string CreateNameForStream(ClusterOptions clusterOptions, QueueId queueId)
     {
-        return $"stream-{clusterOptions.ClusterId}-{clusterOptions.ServiceId}-{queueId.ToString()}";
+        return CreateNameInternal(Stream, clusterOptions.ClusterId, clusterOptions.ServiceId, queueId.ToString());
+    }
+
+    private static string CreateNameInternal(string type, string clusterId, string serviceId, string name)
+    {
+        return $"{type}-{clusterId}-{serviceId}-{name}";
     }
 }
