97d182302f8e4421dad236c6026ab01d3778bd0a
Simon Novak <snovak7@gmail.com>
2023-06-10 21:18:49 +0200

    feature(#2): Introduced ConnectionFactoryBuilder.

---
+++ src/Escendit.Orleans.Streaming.RabbitMQ/Hosting/IRabbitConnectionFactoryBuilder.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) Escendit Ltd. All Rights Reserved.
+// Licensed under the MIT. See LICENSE.txt file in the solution root for full license information.
+
+namespace Escendit.Orleans.Streaming.RabbitMQ.Hosting;
+
+using Microsoft.Extensions.DependencyInjection;
+
+/// <summary>
+/// Rabbit Connection Factory Builder.
+/// </summary>
+public interface IRabbitConnectionFactoryBuilder
+{
+    /// <summary>
+    /// Gets the service collection.
+    /// </summary>
+    /// <value>The services.</value>
+    IServiceCollection Services { get; }
+
+    /// <summary>
+    /// Gets the name.
+    /// </summary>
+    /// <value>The name.</value>
+    string Name { get; }
+}
